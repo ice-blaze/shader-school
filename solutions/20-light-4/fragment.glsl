9ab77ac91e32762ce4a466f3b6a0ab159a50fff4
Ice-Blaze <franketip@gmail.com>
2016-02-29 00:07:35 +0100

    Add lesson 20: point lights

--- solutions/20-light-4/fragment.glsl
+++ solutions/20-light-4/fragment.glsl
@@ -16,6 +16,41 @@ uniform vec3 lightPosition;
 
 uniform float shininess;
 
+varying vec3 vEyeDirection;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+
+float lambertWeight(vec3 n, vec3 d) {
+  return dot(n, d);
+  // correct version but should be an issue with shader school
+  // return max(dot(n, d), 0.0);
+}
+
+float phongWeight(
+  vec3 lightDirection,
+  vec3 surfaceNormal,
+  vec3 eyeDirection,
+  float shininess
+) {
+  //First reflect light by surface normal
+  vec3 rlight = reflect(lightDirection, surfaceNormal);
+
+  //Next find the projected length of the reflected
+  //light vector in the view direction
+  float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+
+  //Finally exponentiate by the shininess factor
+  return pow(eyeLight, shininess);
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  // vPosition = (project * view * model * v4(vPosition,0)).xyz;
+  vec3 lightDirection = vPosition;
+
+  float phong = phongWeight(lightDirection,vNormal,vEyeDirection,shininess);
+  float lambert = lambertWeight(vNormal, lightDirection);
+  vec3 res = ambient + diffuse*lambert + specular*phong;
+  gl_FragColor = vec4(res,1);
+  // gl_FragColor = vec4(1,0,0,1);
 }
--- solutions/20-light-4/vertex.glsl
+++ solutions/20-light-4/vertex.glsl
@@ -13,6 +13,41 @@ uniform mat4 inverseProjection;
 
 uniform vec3 lightPosition;
 
+varying vec3 vEyeDirection;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+float lambertWeight(vec3 n, vec3 d) {
+  return max(dot(n, d), 0.0);
+}
+
+float phongWeight(
+  vec3 lightDirection,
+  vec3 surfaceNormal,
+  vec3 eyeDirection,
+  float shininess
+) {
+  //First reflect light by surface normal
+  vec3 rlight = reflect(lightDirection, surfaceNormal);
+
+  //Next find the projected length of the reflected
+  //light vector in the view direction
+  float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+
+  //Finally exponentiate by the shininess factor
+  return pow(eyeLight, shininess);
+}
+
 void main() {
-  gl_Position = vec4(position, 1);
+  vec4 viewPosition = view * model * vec4(position, 1.0);
+  gl_Position = projection * viewPosition;
+
+  // vPosition = normalize(lightPosition - position);
+  // TODO don't understand why should view* the lightposition and view*model* the position, not symetric why ?
+  vec4 viewLight = view * vec4(lightPosition, 1.0);
+  vPosition = normalize(viewLight.xyz - viewPosition.xyz);
+
+
+  vNormal = normalize((vec4(normal, 0.0) * inverseModel * inverseView).xyz);
+  vEyeDirection = normalize(viewPosition.xyz);
 }
