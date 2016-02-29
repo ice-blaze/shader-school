9337532ceeb121b29e9d0e51e4c84a954a4d58ca
ice-blaze <franketip@gmail.com>
2016-02-29 11:52:58 +0100

    Add lesson 24
    Shader school can't verify the solution ( maybe I should do the lesson before)

--- solutions/24-gpgpu-1/life.glsl
+++ solutions/24-gpgpu-1/life.glsl
@@ -9,10 +9,17 @@ float state(vec2 coord) {
 
 void main() {
   vec2 coord = gl_FragCoord.xy;
-
-
-  //TODO: Compute the next state for the cell at coord
   float s = state(coord);
+  float counter = 0.0;// when declaring float have to 0.0, if vec2 just 0 is ok
 
-  gl_FragColor = vec4(s,s,s, 1.0);
+  for (int i = -1; i <= 1; i++)
+  for (int j = -1; j <= 1; j++) {
+    counter += state(coord + vec2(i, j));
+  }
+
+  if(counter > 3.0+s || counter < 3.0){
+    gl_FragColor = vec4(0,0,0, 1.0);
+  }else{
+    gl_FragColor = vec4(1,1,1, 1.0);
+  }
 }
