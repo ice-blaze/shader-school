29f60201fa34d95f52796e6056da3e1d4e9ddc87
ice-blaze <franketip@gmail.com>
2016-03-01 13:06:40 +0100

    Lesson 22

--- solutions/22-npr-1/fragment.glsl
+++ solutions/22-npr-1/fragment.glsl
@@ -12,6 +12,20 @@ uniform vec3 diffuse;
 uniform vec3 lightDirection;
 uniform float numBands;
 
+varying vec3 worldNormal;
+
+float lambertWeight(vec3 n, vec3 d) {
+  return max(dot(n, d), 0.0);
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+
+  float lambert = lambertWeight(worldNormal, lightDirection);
+
+  float celIntensity = ceil(lambert * numBands) / numBands;
+
+  vec3 res = diffuse*celIntensity;
+
+  gl_FragColor = vec4(res,1);
+  // gl_FragColor = vec4(1,1,1,1);
 }
