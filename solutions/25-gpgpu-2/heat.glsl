7abe5ecd6db95e86417e663c39c1153697cf8d2e
ice-blaze <franketip@gmail.com>
2016-03-01 13:07:18 +0100

    Lesson 25

--- solutions/25-gpgpu-2/heat.glsl
+++ solutions/25-gpgpu-2/heat.glsl
@@ -9,13 +9,26 @@ float state(vec2 x) {
   return texture2D(prevState, fract(x / stateSize)).r;
 }
 
+float state(float x1, float y1) {
+  vec2 x = vec2(x1,y1);
+  return texture2D(prevState, fract(x / stateSize)).r;
+}
+
+float laplacian(vec2 v) {
+  return (
+    state(v.x-1.,v.y) +
+    state(v.x+1.,v.y) +
+    state(v.x,v.y-1.) +
+    state(v.x,v.y+1.)
+  ) - 4.0 * state(v.x,v.y);
+}
+
 void main() {
   vec2 coord = gl_FragCoord.xy;
 
-  //TODO: Compute next state using a 5-point Laplacian stencil and the rule
-
-  float y = state(coord);
-
+  float laplace = laplacian(coord);
+  float preveState = state(coord);
+  float y = (1.0 - kdamping) * (kdiffuse*laplace + preveState);
 
   gl_FragColor = vec4(y,y,y,1);
 }
