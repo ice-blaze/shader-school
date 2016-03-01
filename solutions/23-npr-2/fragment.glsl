62d8b45a04a9e4154f32b44fa9050f58c296b3ca
ice-blaze <franketip@gmail.com>
2016-03-01 13:07:03 +0100

    Lesson 23

--- solutions/23-npr-2/fragment.glsl
+++ solutions/23-npr-2/fragment.glsl
@@ -12,6 +12,26 @@ uniform vec3 warm;
 uniform vec3 cool;
 uniform vec3 lightDirection;
 
+varying vec3 worldNormal;
+
+float lambertWeight(vec3 n, vec3 d) {
+  return max(dot(n, d), 0.0);
+}
+
+float goochWeight(vec3 normal, vec3 lightDirection) {
+  return 0.5 * (1.0 + dot(normal, lightDirection));
+}
+
+vec3 goochColor(vec3 cool, vec3 warm, float weight) {
+  return (1.0 - weight) * cool + weight * warm;
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+
+  float goochW = goochWeight(worldNormal, lightDirection);
+
+  vec3 res = goochColor(cool,warm,goochW);
+
+  gl_FragColor = vec4(res,1);
+  // gl_FragColor = vec4(1,1,1,1);
 }
