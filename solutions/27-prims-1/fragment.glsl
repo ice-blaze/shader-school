cd816d239ccb618c3165f20f388a862f1dde2446
ice-blaze <franketip@gmail.com>
2016-03-01 13:07:41 +0100

    Lesson 27

--- solutions/27-prims-1/fragment.glsl
+++ solutions/27-prims-1/fragment.glsl
@@ -1,5 +1,24 @@
 precision highp float;
 
+varying vec3 vColor;
+varying float vSize;
+
+bool isInCircle(){
+  float dx = gl_PointCoord.x - 0.5;
+  float dy = gl_PointCoord.y - 0.5;
+  if( dx*dx + dy*dy >= 0.5*0.5){
+    return true;
+  }
+  return false;
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+
+  if(isInCircle()){
+    //set the alpha didn't work for points
+    discard;
+  }
+
+  gl_FragColor = vec4(vColor,1.);
+
 }
