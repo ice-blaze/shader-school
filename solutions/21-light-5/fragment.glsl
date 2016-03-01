9ab51eddc9862549755793daacd34e2f6ba17678
ice-blaze <franketip@gmail.com>
2016-03-01 13:06:23 +0100

    Lesson 21

--- solutions/21-light-5/fragment.glsl
+++ solutions/21-light-5/fragment.glsl
@@ -12,8 +12,47 @@ uniform mat4 inverseProjection;
 
 uniform vec3 ambient;
 
+const int NB_LIGHTS = 4;
 uniform PointLight lights[4];
 
+varying vec3 vNormal;
+varying vec3 vEyeDirection;
+varying vec3 lightsDirection[4];
+
+float lambertWeight(vec3 n, vec3 d) {
+  return dot(n, d);
+  // correct version but should be an issue with shader school
+  // return max(dot(n, d), 0.0);
+}
+
+float phongWeight(
+  vec3 lightDirection,
+  vec3 surfaceNormal,
+  vec3 eyeDirection,
+  float shininess
+) {
+  //First reflect light by surface normal
+  vec3 rlight = reflect(lightDirection, surfaceNormal);
+
+  //Next find the projected length of the reflected
+  //light vector in the view direction
+  float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+
+  //Finally exponentiate by the shininess factor
+  return pow(eyeLight, shininess);
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+
+  vec3 res = ambient;
+
+  for(int i=0;i<NB_LIGHTS;i++){
+    float phong = phongWeight(lightsDirection[i],vNormal,vEyeDirection,lights[i].shininess);
+
+    float lambert = lambertWeight(vNormal, lightsDirection[i]);
+
+    res += lights[i].diffuse*lambert + lights[i].specular*phong;
+  }
+
+  gl_FragColor = vec4(res,1);
 }
--- solutions/21-light-5/vertex.glsl
+++ solutions/21-light-5/vertex.glsl
@@ -15,8 +15,45 @@ uniform mat4 inverseProjection;
 
 uniform vec3 ambient;
 
+const int NB_LIGHTS = 4;
 uniform PointLight lights[4];
 
+varying vec3 vNormal;
+varying vec3 vEyeDirection;
+varying vec3 lightsDirection[4];
+
+float lambertWeight(vec3 n, vec3 d) {
+  return max(dot(n, d), 0.0);
+}
+
+float phongWeight(
+  vec3 lightDirection,
+  vec3 surfaceNormal,
+  vec3 eyeDirection,
+  float shininess
+) {
+  //First reflect light by surface normal
+  vec3 rlight = reflect(lightDirection, surfaceNormal);
+
+  //Next find the projected length of the reflected
+  //light vector in the view direction
+  float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+
+  //Finally exponentiate by the shininess factor
+  return pow(eyeLight, shininess);
+}
+
 void main() {
-  gl_Position = vec4(position, 1);
+  vec4 viewPosition = view * model * vec4(position, 1.0);
+  gl_Position = projection * viewPosition;
+
+  // vLightDirection = normalize(lightPosition - position);
+  // TODO don't understand why should view* the lightposition and view*model* the position, not symetric why ?
+  for(int i=0;i<NB_LIGHTS;i++){
+    vec4 viewLight = view * vec4(lights[i].position, 1.0);
+    lightsDirection[i] = normalize(viewLight.xyz - viewPosition.xyz);
+  }
+
+  vNormal = normalize((vec4(normal, 0.0) * inverseModel * inverseView).xyz);
+  vEyeDirection = normalize(viewPosition.xyz);
 }
